c18b573eae249635a3bfc1ac590ed6e4bc3010ed
Zach Steindler <steiza@coffeehousecoders.org>
2018-10-27 14:45:51 -0400

    bump version number to prepare for release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='simplepypi',
-        version='0.1.5',
+        version='0.1.6',
         author='Zach Steindler',
         author_email='steiza@coffeehousecoders.org',
         url='http://github.com/steiza/simplepypi',
