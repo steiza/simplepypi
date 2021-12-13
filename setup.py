f0906b6c4880b8481c72d62c41cd94595d900091
Zach Steindler <steiza@coffeehousecoders.org>
2021-12-13 08:10:17 -0500

    Normalize package name per PEP 503:
    https://www.python.org/dev/peps/pep-0503/#normalized-names

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='simplepypi',
-        version='0.2.0',
+        version='0.2.1',
         author='Zach Steindler',
         author_email='steiza@coffeehousecoders.org',
         url='http://github.com/steiza/simplepypi',
